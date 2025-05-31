08c8f23c9c016464e6104913a5a53bdc382136f1
salmansheri <sheriffsalman00@gmail.com>
2025-05-31 13:50:47 +0530

    Added Prettier

--- src/frontend/src/routes/__root.tsx
+++ src/frontend/src/routes/__root.tsx
@@ -14,8 +14,6 @@ interface MyRouterContext {
 export const Route = createRootRouteWithContext<MyRouterContext>()({
   component: () => (
     <>
-  
-
       <Outlet />
       {/* <TanStackRouterDevtools />
 
